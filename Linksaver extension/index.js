0286d2c5186eaeb2bb7a2db0b1932d6f250bc29d
Jia <jsiangfung@gmail.com>
2021-12-06 11:45:38 -0800

    added event on window close/updated local storage saving

--- Linksaver extension/index.js
+++ Linksaver extension/index.js
@@ -7,29 +7,57 @@ let storedLinksEl = document.getElementById("saved-links");
 let storedLinks = [];
 
 
-//  restoreSavedLinks();
-//  printStoredLinks();    
-
+formatSavedLinks(); 
+printStoredLinks(); 
+ 
 inputBtn.addEventListener("click", function(){
     saveLink();
-    //linkLocalStorage();
+    linkLocalStorage();
 });
 
+//Saves current storedLinks to local storage on exit
+window.addEventListener("close", function(){
+    linkLocalStorage();
+})
 
+//Saves link in input text field 
 function saveLink(){
     let link = linkAdr.value; 
     storedLinks.push(link); 
     printStoredLinks();
 }
 
-// function linkLocalStorage(){
-//     localStorage.clear(); 
-//     localStorage.setItem("saved-links",  JSON.stringify(storedLinks));   
-// }
+//Deletes most recent link from history - and updates storage
+function deleteLink(){
+    storedLinks.pop(); 
+    printStoredLinks(); 
+    //linkLocalStorage();
+}
 
-// function restoreSavedLinks(){
-//     storedLinks = localStorage.getItem("saved-links");
-// }
+//Deletes all links from history and storage 
+function deleteAllLinks(){
+    storedLinks = [];
+    storedLinksEl.innerText = ""; 
+    //localStorage.clear(); 
+}
+
+//Saves currently stored links to local storage. Clears old data from storage
+function linkLocalStorage(){
+    localStorage.clear(); 
+    localStorage.setItem("saved-links",  JSON.stringify(storedLinks));   
+}
+
+
+
+
+//Cleans up the stored links from JSON string form and convert it to a string array to set storedLinks to
+function formatSavedLinks(){
+    let unformattedLinks = localStorage.getItem("saved-links"); //One giant string for saved links in storage
+    //String.splice() - Returns string in between two indexes of referenced string
+    let formattedLinks = unformattedLinks.slice(1, unformattedLinks.length-1); //Returns the string between the array brackets 
+    storedLinks = formattedLinks.split(',');  //Splits string into an array with entries based on delimiter ','
+    console.log(storedLinks); 
+}
 
 function printStoredLinks(){
     if(storedLinks.length != 0){
@@ -47,7 +75,7 @@ function printStoredLinks(){
             // li.textContent = storedLinks[i]; 
             // storedLinksEl.append(li);
         }
-        storedLinksEl.innerHTML = listItems;
+        storedLinksEl.innerText = listItems;
     }
 }
 
