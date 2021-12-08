99ec8faa6eb86908d25be4ffee8decd74b529286
Jia <jsiangfung@gmail.com>
2021-12-08 11:37:53 -0800

    fixed bug with local storage save/more QoL

--- Linksaver extension/index.js
+++ Linksaver extension/index.js
@@ -12,24 +12,20 @@ let storedLinksEl = document.getElementById("saved-links");
 
 let storedLinks = [];
 
-//TODO: Null references to storedLink causing bugs.
-
-//Restores links from local storage at start - if storedLinks had nothing saved storedLinks will be set to NULL so we have to reinitialize it to empty array
-formatSavedLinks();
-// if (storedLinks === null) {
-//   storedLinks = [];
-// } else{
-//   console.log("undefined: false");
-// }
+//Restores links from local storage at start, if there are any.
+if (localStorage.getItem("saved-links") != null) {
+  //One giant string for saved links in storage)
+  let unformattedLinks = localStorage.getItem("saved-links");
+  storedLinks = JSON.parse(unformattedLinks); 
+}
 
 printStoredLinks();
 
 //Saves link to storage and updates history
 inputBtn.addEventListener("click", function () {
-  saveLink(); 
+  saveLink();
   linkLocalStorage();
-  console.log(storedLinks); 
-  
+  console.log(storedLinks);
 });
 
 let tabs = [{ url: "" }];
@@ -40,32 +36,28 @@ tabBtn.addEventListener("click", function () {
     storedLinks.push(tabs[0].url);
     linkLocalStorage();
     printStoredLinks();
-    console.log(storedLinks); 
+    console.log(storedLinks);
   });
 });
 
 //Deletes most recent link in storage and updates history
 deleteBtn.addEventListener("click", function () {
   deleteLink();
+  linkLocalStorage();
 });
 
 //Deletes all links in storage and updates history
 deleteAllBtn.addEventListener("click", function () {
   deleteAllLinks();
-});
-
-//Saves current storedLinks to local storage on exit/refresh - saves memory doing it once at the end vs doing it on every button click.
-//Makes sense to do it once at the end too since we're saving the storage when the user is finished for next time.
-window.addEventListener("beforeunload", function () {
   linkLocalStorage();
 });
 
 //Saves link in input text field
 function saveLink() {
   let link = linkAdr.value;
-  if(link != ""){ 
+  if (link != "") {
     storedLinks.push(link);
-    linkAdr.value = ""; 
+    linkAdr.value = "";
     printStoredLinks();
   }
 }
@@ -89,29 +81,17 @@ function linkLocalStorage() {
   localStorage.setItem("saved-links", JSON.stringify(storedLinks));
 }
 
-//Cleans up the stored links from JSON string form and convert it to a string array to set storedLinks to
-function formatSavedLinks() {
-  let unformattedLinks = localStorage.getItem("saved-links"); //One giant string for saved links in storage
-  //Turns out this code wasn't needed since I can just use JSON.parse()...XD
-  // //String.slice() - Returns string in between two indexes of referenced string
-  // let formattedLinks = unformattedLinks.slice(1, unformattedLinks.length-1); //Returns the string between the array brackets
-  // storedLinks = formattedLinks.split(',');  //Splits string into an array with entries based on delimiter ','
-  // console.log(storedLinks);
-  //  Yo I think the bug from earlier with extra quotations was cause we removed the brackets but not the quotation "" marks from each string entry... and so
-  //  the quotations were counted as a part of the string. Every time we saved the entry with quotations is wrapped inside another quotation mark. Wow.
-  storedLinks = JSON.parse(unformattedLinks);
-}
-
 //Updates link history with links in storage.
 function printStoredLinks() {
   let listItems = "";
+
   for (let i = 0; i < storedLinks.length; i++) {
     listItems += `
-            <li>
-                <a id='saved-links' target='_blank' href='${storedLinks[i]}'> 
-                 ${storedLinks[i]} 
-                </a>
-            </li>`;
+              <li>
+                  <a id='saved-links' target='_blank' rel='noreferrer noopener' href='${storedLinks[i]}'> 
+                   ${storedLinks[i]} 
+                  </a>
+              </li>`;
 
     // Does the same thing and is less intuitive to understand
     // const li = document.createElement("li");
@@ -121,3 +101,31 @@ function printStoredLinks() {
 
   storedLinksEl.innerHTML = listItems;
 }
+
+//Bug log/ Problems faced: 
+//Cleans up the stored links from JSON string form and convert it to a string array to set storedLinks to
+// function formatSavedLinks() {
+//   //Turns out this code wasn't needed since I can just use JSON.parse()...XD
+//   // //String.slice() - Returns string in between two indexes of referenced string
+//   // let formattedLinks = unformattedLinks.slice(1, unformattedLinks.length-1); //Returns the string between the array brackets
+//   // storedLinks = formattedLinks.split(',');  //Splits string into an array with entries based on delimiter ','
+//   // console.log(storedLinks);
+//   //  Yo I think the bug from earlier with extra quotations was cause we removed the brackets but not the quotation "" marks from each string entry... and so
+//   //  the quotations were counted as a part of the string. Every time we saved the entry with quotations is wrapped inside another quotation mark. Wow.
+//   return JSON.parse(unformattedLinks);
+// }
+
+//Saves current storedLinks to local storage on exit/refresh - saves memory doing it once at the end vs doing it on every button click.
+//Makes sense to do it once at the end too since we're saving the storage when the user is finished for next time.
+//Doesn't cover the case where we switch tabs (not exiting/refreshing) so not using this function anymore
+// window.addEventListener("beforeunload", function () {
+//   linkLocalStorage();
+// });
+
+//Restores links from local storage at start, if there are any.
+// Gotta test for .getItem edge case where there is nothing to pull from since we'll be setting storedLinks to a null object
+// if (localStorage.getItem("saved-links") != null) {
+//   //One giant string for saved links in storage)
+//   let unformattedLinks = localStorage.getItem("saved-links");
+//   storedLinks = JSON.parse(unformattedLinks); 
+// }
